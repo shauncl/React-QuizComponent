7bec1624f024444bf307ca5efd884f3c7d2875e2
Shaun Lavery <shaun.lavery@code42.com>
2018-10-31 13:37:39 -0500

    Completed the module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Quiz from './Quiz'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Quiz />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
